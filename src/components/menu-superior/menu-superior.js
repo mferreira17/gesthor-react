2c7502a0c3a6364d659d0e403564c9ecc7fc8dd7
Márcio Ferreira <mwferreira17@gmail.com>
2021-02-08 16:31:14 -0300

    Update menu-superior.js

--- src/components/menu-superior/menu-superior.js
+++ src/components/menu-superior/menu-superior.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { BrowserRouter as Router , Link} from 'react-router-dom';
+
 
 function LoginButton() {
 	return(
@@ -10,25 +12,31 @@ function LoginButton() {
 function MenuSuperior(){
 	
 	return (
+		<Router>
 		<Navbar expand="lg" bg="dark" variant="dark">
 			<Navbar.Brand>Gesthor</Navbar.Brand>
 			<Navbar.Toggle aria-controls="navbar-nav-menusuperior" />
 			<Navbar.Collapse>
 			  <Nav className="mr-auto">
 				<NavDropdown id="navbar-nav-menusuperior" title="Contratos">
-					<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+					<NavDropdown.Item>
+						<Link style={{color:'black', textDecoration:'none'}} to="/novo">
+						&nbsp;
+						<i className="bi bi-file-plus" style={{textIndent:'center'}}></i>
+							Novo Contrato 
+						</Link>
+					</NavDropdown.Item>
 				</NavDropdown>
 				<NavDropdown id="navbar-nav-menusuperior" title="Ocorrências">
 					<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
 				</NavDropdown>
 				</Nav>
 				<Nav>
-					<Nav.Link href="#">
-						<LoginButton/>
-					</Nav.Link>
-		        </Nav>
+					<Link to="/sobre"><LoginButton/></Link>
+				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
+		</Router>
 	);
 	
 }
